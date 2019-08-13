0e72be98b304d7e18b2a65d7d4d3a046fb34c038
lgsmd <584588192@qq.com>
2019-08-13 18:17:26 +0800

    unit test -- the menu is hidden

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -48,17 +48,26 @@ $(function() {
 
 
     /* TODO: 写一个叫做 "The menu" 的测试用例 */
-
+    describe('The menu', function () {
         /* TODO:
          * 写一个测试用例保证菜单元素默认是隐藏的。你需要分析 html 和 css
          * 来搞清楚我们是怎么实现隐藏/展示菜单元素的。
          */
+        let elem = document.body.querySelector('.slide-menu');
+        let style = elem.getBoundingClientRect();   //获取元素位置信息
 
-         /* TODO:
-          * 写一个测试用例保证当菜单图标被点击的时候菜单会切换可见状态。这个
-          * 测试应该包含两个 expectation ： 党点击图标的时候菜单是否显示，
-          * 再次点击的时候是否隐藏。
-          */
+        it('are hidden menu', function () {
+            expect(style.left + style.width <= 0).toBe(true);
+        })
+
+        /* TODO:
+         * 写一个测试用例保证当菜单图标被点击的时候菜单会切换可见状态。这个
+         * 测试应该包含两个 expectation ： 党点击图标的时候菜单是否显示，
+         * 再次点击的时候是否隐藏。
+         */
+
+    })
+
 
     /* TODO: 13. 写一个叫做 "Initial Entries" 的测试用例 */
 
